365936ffa561d3d85ebfaf4236e4cd20b75ec015
myklt <maiklasm@gmail.com>
2017-01-21 23:32:48 +0100

    Enable hot replacements for dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+const webpack = require('webpack');
+
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: __dirname,
         filename: "bundle.js",
         publicPath: '/dist/'
-    }
+    },
+    devServer: {
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
